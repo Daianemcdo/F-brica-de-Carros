7b3c4fbfae20699cd02ba7dcb2263eeb5b2136f5
Daianemcdo <31110728+Daianemcdo@users.noreply.github.com>
2020-08-02 14:25:12 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,51 @@
+package POO;
+
+public class Carro {
+	
+	String modelo;
+	String cor;
+	double motor;
+	double velocidadeAtual;
+	double velocidadeMaxima;
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public double getMotor() {
+		return motor;
+	}
+	
+	public void setMotor(double motor) {
+		this.motor = motor;
+	}
+	
+	public void setVelocidadeAtual(double velocidadeAtual) {
+		this.velocidadeAtual = velocidadeAtual;
+	}
+	
+	public double getVelocidadeAtual() {
+		return velocidadeAtual;
+	}
+	
+	public void setVelocidadeMaxima(double velocidadeMaxima) {
+		this.velocidadeMaxima = velocidadeMaxima;
+	}
+	
+	public double getVelocidadeMaxima() {
+		return velocidadeMaxima;
+	}
+}
+	
---
+++ SistemaCarro.java
@@ -0,0 +1,26 @@
+package POO;
+
+public class SistemaCarro {
+	
+	public static void main(String args[] ) {
+	
+/*	public void ligar() {
+		boolean velocidadeAtual = true;
+		if (velocidadeAtual == true) {
+			System.out.println("Carro está ligado");
+		}
+		else {
+			System.out.println("Carro está desligado");
+		}
+	}
+*/
+	}
+		public void ligar() {
+	        boolean ligado = false;
+			if (ligado == false) {
+	            System.out.println("Ligando...");
+	            ligado = true;
+			}
+		}
+
+}
